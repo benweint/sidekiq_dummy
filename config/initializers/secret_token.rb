e2715f4904519b43cedf3c622dde326d86b8d984
Ben Weintraub <ben@newrelic.com>
2013-11-13 07:57:36 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SidekiqDummy::Application.config.secret_key_base = 'd755c051f6e9d99053177b7df9f04c72c7b98cf534cb78b038d8cbc0b0445d0a3b8623f301ca261fbc535057cc78418e9b12dd465900b4f403acd8e217a1705e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SidekiqDummy::Application.config.session_store :cookie_store, key: '_sidekiq_dummy_session'
