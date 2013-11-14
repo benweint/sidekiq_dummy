8e1f6481f85aa3f4152725564ef2271bc20544a6
Ben Weintraub <ben@newrelic.com>
2013-11-13 17:02:24 -0800

    Silly hack to require an exports file

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,12 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+exports_path = File.expand_path('./exports', File.dirname(__FILE__))
+
+if File.exist?(exports_path)
+  require exports_path
+else
+  puts "Did not file exports file at '#{exports_path}'"
+end
+
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
